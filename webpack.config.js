85c88a06f54fc3c667695d5a9122f69185a1b619
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-31 18:14:14 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/i,
+        loader: 'ejs-loader',
+        options: {
+          esModule: false,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/style.css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Calendar app',
+      hash: true,
+      minify: false,
+      template: path.resolve(__dirname, 'src', 'index.ejs'),
+      filename: path.resolve(__dirname, 'dist', 'index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
