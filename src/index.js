a8bab2d624d7aa4d3144ebd95eceacbf75a3d975
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-01-31 18:56:31 +0200

    bootstrap include & start markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
 import './js/calendar';
---
+++ src/js/_data.js
@@ -0,0 +1,9 @@
+const DATA = {
+  calendar: {
+    a: '1',
+  },
+};
+
+const foo = 'bar';
+
+export { DATA, foo };
