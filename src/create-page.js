01b6b11655f0304cff45453192809bfec8480993
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-03-05 23:32:46 +0200

    removied all besides doc-images

--- src/create-page.js
+++
@@ -1,9 +0,0 @@
-import '@babel/polyfill';
-import './base-import';
-
-import './js/createEvent';
-import './js/moveParticipants';
-
-import loadUsers from './js/allUsers';
-
-loadUsers();
--- src/js/User.js
+++
@@ -1,7 +0,0 @@
-export default class User {
-  constructor(id, name, avatar) {
-    this.id = id;
-    this.name = name;
-    this.avatar = avatar;
-  }
-}
