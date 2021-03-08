b2e0087ec59679c2682ec8a228339ff9ee0e1d99
Sergey-Nag <nagornyj.seriozha2010@gmail.com>
2021-03-08 04:23:40 +0200

    last takes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
   "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest/globals": true
-    },
-    "extends": [
-        "airbnb-base",
-        "prettier"
-    ],
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-      "import/no-named-as-default": 0,
-      "import/no-named-as-default-member": 0,
-      "max-classes-per-file": ["error", 3]
-    },
-    "plugins": ["jest"]
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jest/globals": true
+  },
+  "extends": [
+    "airbnb-base",
+    "prettier"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
+    "max-classes-per-file": ["error", 3]
+  },
+  "plugins": ["jest"]
 };
